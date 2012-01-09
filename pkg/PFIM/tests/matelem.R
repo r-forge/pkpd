f3b42ff02c13d1576cc76790ee984499ee1d6a50
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2012-01-09 19:06:46 +0000

    Expanded the output from the test.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pkpd@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/PFIM/tests/matelem.R
+++ pkg/PFIM/tests/matelem.R
@@ -6,10 +6,18 @@ close(con)
 
 prot$freq()                    # initialized to single protocol with maximum determinant
 which(as.logical(prot$freq())) # 1-based index of protocol with maximum determinant
-prot$ldet()                    # logarithm of that determinant
-prot$Fim()                     # Fisher Information matrix for this (trivial) combination
+prot$props()                   # another way of getting the same
 
-prot$ajout()                   # try to add another but no other improves it
+op <- options(digits=3)        # more concise printing of numeric values
+prot$Fim()                     # Fisher Information matrix for these (trivial) proportions
+prot$LMat()                    # Cholesky factor of the FIM
+options(op)                    # restore the previous value
+
+all.equal(tcrossprod(prot$LMat()), prot$Fim()) # check that LL' = F
+prot$ldet()                    # log-determinant of the FIM
+2 * log(prod(diag(prot$LMat()))) # another way of calculating the log-determinant of FIM
+
+prot$ajout()                   # try to add another individual protocol but no other improves it
 
 unzip(system.file(package="PFIM", "Examples", "ECo", "matelem1.zip"), "matelem1.tmp")
 con <- file("matelem1.tmp", open="r")
@@ -24,9 +32,9 @@ while (prot$ajout() && length(prot$props()) < 100L) {} # add up to 99 protocols
 
 op <- options(digits=3)
 rev(sort(prot$props()))        # combination (decreasing coefficients)
+prot$Fim()                     # Fisher Information matrix
 options(op)
 
-prot$Fim()                     # Fisher Information matrix
 prot$ldet()                    # log-determinant for FIM with 100 non-zeros
 
 q('no')
